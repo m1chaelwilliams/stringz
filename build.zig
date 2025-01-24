9f7792a84eb8008c65e101a2a1325a73aff1efed
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@gmail.com>
2025-01-24 00:47:38 -0800

    build.zig fix attempt

--- build.zig
+++ build.zig
@@ -4,18 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.addModule("stringz", .{
+    _ = b.addModule("stringz", .{ .root_source_file = b.path("src/root.zig") });
+
+    const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&lib_unit_tests.step);
 }
